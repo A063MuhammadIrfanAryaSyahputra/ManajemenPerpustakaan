f47cfbd3c4c58d33d56c9406139ca52e4a80f71a
A063MuhammadIrfanAryaSyahputra <muhammad2100018063@webmail.uad.ac.id>
2024-05-04 21:06:25 +0700

    Test 1
    Dashboard

--- footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-    <a href="https://xlmdlx.xyz/about" class="footermain">
-        <span class="spin">&copy;</span> 2024 forssellfritz/khindy zharifany, attribution
-    </a>
-</div>
