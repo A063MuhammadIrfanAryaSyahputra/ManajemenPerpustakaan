8d471865676aca8b2c2072610f8530d835a684d3
Khindy Zharifany <zharifanny@gmail.com>
2024-06-21 23:48:04 +0700

    add navbar alignment fix

--- Main/navbarAmansa.php
+++ Main/navbarAmansa.php
@@ -1,50 +1,50 @@
 <nav>
-    <div class="logo">
-      <a href="index.php"><img src="assets/logoHorizon.png" alt="logo" /></a>
-    </div>
-    </div>
-    <ul>
-      <li>
-        <a href="index.php">Beranda</a>
-      </li>
-      <li>
-        <a href="#">Tentang Amansa</a>
-      </li>
-      <li>
-        <a href="Umroh.php">Umroh</a>
-      </li>
-      <li>
-        <a href="Haji.php">Haji</a>
-      </li>
-      <li>
-        <a href="#">Badal</a>
-      </li>
-      <li>
-        <a href="#">Kemitraan</a>
-      </li>
-      <li>
-        <a href="galeri.php">Galeri & Artikel</a>
-      </li>
-    </ul>
-    <div class="hamburger">
-      <span class="line"></span>
-      <span class="line"></span>
-      <span class="line"></span>
-    </div>
-  </nav>
-  <div class="menubar">
-    <ul>
-      <li>
-        <a href="#">Home</a>
-      </li>
-      <li>
-        <a href="#">Services</a>
-      </li>
-      <li>
-        <a href="#">Blog</a>
-      </li>
-      <li>
-        <a href="#">Contact Us</a>
-      </li>
-    </ul>
+  <div class="logo">
+    <a href="index.php"><img src="assets/logoHorizon.png" alt="logo" /></a>
   </div>
+  </div>
+  <ul>
+    <li>
+      <a href="index.php">Beranda</a>
+    </li>
+    <li>
+      <a href="#">Tentang Amansa</a>
+    </li>
+    <li>
+      <a href="Umroh.php">Umroh</a>
+    </li>
+    <li>
+      <a href="Haji.php">Haji</a>
+    </li>
+    <li>
+      <a href="#">Badal</a>
+    </li>
+    <li>
+      <a href="#">Kemitraan</a>
+    </li>
+    <li>
+      <a href="galeri.php">Galeri & Artikel</a>
+    </li>
+  </ul>
+  <div class="hamburger">
+    <span class="line"></span>
+    <span class="line"></span>
+    <span class="line"></span>
+  </div>
+</nav>
+<div class="menubar">
+  <ul>
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">Services</a>
+    </li>
+    <li>
+      <a href="#">Blog</a>
+    </li>
+    <li>
+      <a href="#">Contact Us</a>
+    </li>
+  </ul>
+</div>
