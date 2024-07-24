bda9915560ae9afef0a521490a3d68e32cf20df9
Khindy Zharifany <zharifanny@gmail.com>
2024-07-24 18:01:23 +0700

    add question

--- footerIndex.php
+++ footerIndex.php
@@ -26,10 +26,26 @@
             </div>
 
             <div class="footer-logo" style="flex: 1; display: flex; align-items: center;">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#E0B64A" class="bi bi-envelope" viewBox="0 0 20 20" style="flex-shrink: 0;">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
+                </svg>
+                <div class="logo-description" style="margin-left: 20px;">
+                    <!-- <a href="mailto:amansatourstravel@gmail.com" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">amansatourstravel@gmail.com</a> -->
+                    <a href="main/questionForm.php" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">Ada Pertanyaan?</a>
+                    <!-- <a href="questionForm.html" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">Question Form</a> -->
+
+                </div>
+            </div>
+
+
+
+            <div class="footer-logo" style="flex: 1; display: flex; align-items: center;">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#E0B64A" class="bi bi-envelope" viewBox="0 0 20 20" style="flex-shrink: 0;">
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                 </svg>
-                <div class="logo-description" style="margin-left: 10px;">
+                <div class="logo-description" style="margin-left: 20px;">
                     <!-- <a href="mailto:amansatourstravel@gmail.com" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">amansatourstravel@gmail.com</a> -->
                     <a href="main/questionForm.php" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">amansatourstravel@gmail.com</a>
                     <!-- <a href="questionForm.html" style="margin-bottom: 10px; font-style: italic; font-size: 16px; color: #E0B64A;">Question Form</a> -->
